91b87d557488e5d665c0a8c4720fb146609765c4
omegaui <omegaui22@gmail.com>
2022-12-31 01:02:40 +0530

    Public Preview: 🚀 Animated GUI Mode!

--- lvc_gui_flutter/lib/ui/app_indicator.dart
+++ lvc_gui_flutter/lib/ui/app_indicator.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:lvc_gui_flutter/constantsx.dart';
 import 'package:lvc_gui_flutter/main.dart';
 
 class AppIndicator extends StatelessWidget{
@@ -7,13 +8,37 @@ class AppIndicator extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    bool listening = tag == ConstantsX.listeningTag;
     return AnimatedContainer(
       duration: const Duration(milliseconds: 200),
-      width: 30,
-      height: 30,
+      width: !listening ? 24 : 30,
+      height: !listening ? 24 : 30,
       decoration: BoxDecoration(
         color: inferTagColor(),
-        borderRadius: BorderRadius.circular(40),
+        borderRadius: BorderRadius.circular(listening ? 10 : 40),
+      ),
+      child: Center(
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 200),
+          transitionBuilder: (child, animation) => FadeTransition(opacity: animation, child: child),
+          child: listening ? Container(
+            key: const Key('box2'),
+            width: 20,
+            height: 20,
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ) : Container(
+            key: const Key('box'),
+            width: 18,
+            height: 18,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
       ),
     );
   }
