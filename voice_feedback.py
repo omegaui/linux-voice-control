5984fd77baf6779680458be3a50016c30a4443ae
omegaui <omegaui22@gmail.com>
2023-01-27 21:23:50 +0530

    Public Preview: 🚀 Improved Performance! 📢 Voice Pitch is now adjustable.

--- voice_feedback.py
+++ voice_feedback.py
@@ -10,6 +10,12 @@ import config_manager
 import notifier
 
 internet = False
+
+wroteLiveModeSpeech = False
+wroteExecutionSpeech = False
+wroteExitingSpeech = False
+wroteTranscriptionSpeech = False
+
 player = None
 try:
     player = mpv.MPV(ytdl=True)  # using mpv
@@ -28,6 +34,7 @@ def speak(text, wait=False):
     if not config_manager.config['voice-feedback-enabled']:
         return
     player.speed = config_manager.config['voice-feedback-speed']
+    player.af = f'rubberband=pitch-scale={config_manager.config["voice-pitch"]}'
     try:
         speech = gTTS(text=text, lang='en', slow=False)
         speech.save('misc/last-feedback-speech.mp3')
@@ -51,6 +58,7 @@ def speak(text, wait=False):
 # internal function to create default voice feedbacks
 def _speak_and_save(text, filename):
     player.speed = config_manager.config['voice-feedback-speed']
+    player.af = f'rubberband=pitch-scale={config_manager.config["voice-pitch"]}'
     try:
         speech = gTTS(text=text, lang='en', slow=False)
         speech.save(filename)
@@ -62,57 +70,68 @@ def _speak_and_save(text, filename):
 def give_execution_feedback():
     if len(config_manager.config['voice-feedback-default-speeches']) == 0:
         return
-    if internet:
+    global wroteExecutionSpeech
+    if internet and not wroteExecutionSpeech:
         speech = speak(random.choice(config_manager.config['voice-feedback-default-speeches']), wait=True)
         if config_manager.config['voice-cache-enabled']:
             speech.save('misc/execution-feedback.mp3')
+            wroteExecutionSpeech = True
     elif os.path.exists('misc/execution-feedback.mp3'):
         player.speed = config_manager.config['voice-feedback-speed']
+        player.af = f'rubberband=pitch-scale={config_manager.config["voice-pitch"]}'
         player.play('misc/execution-feedback.mp3')
         player.wait_for_playback()
 
 
 # voice feedback when exiting
 def give_exiting_feedback():
-    if internet:
+    global wroteExitingSpeech
+    if internet and not wroteExitingSpeech:
         speech = speak(config_manager.config['voice-feedback-turning-off'], wait=True)
         if config_manager.config['voice-cache-enabled']:
             speech.save('misc/exiting-feedback.mp3')
+            wroteExitingSpeech = True
     elif os.path.exists('misc/exiting-feedback.mp3'):
         player.speed = config_manager.config['voice-feedback-speed']
+        player.af = f'rubberband=pitch-scale={config_manager.config["voice-pitch"]}'
         player.play('misc/exiting-feedback.mp3')
         player.wait_for_playback()
 
 # voice feedback when initializing live mode
 def give_live_mode_feedback():
-    if internet:
-        speech = speak("initializing live mode ...", wait=True)
+    global wroteLiveModeSpeech
+    if internet and not wroteLiveModeSpeech:
+        speech = speak("sleeping in the background ...", wait=False)
         if config_manager.config['voice-cache-enabled']:
             speech.save('misc/live_mode-feedback.mp3')
+            wroteLiveModeSpeech = True
     elif os.path.exists('misc/live_mode-feedback.mp3'):
         player.speed = config_manager.config['voice-feedback-speed']
+        player.af = f'rubberband=pitch-scale={config_manager.config["voice-pitch"]}'
         player.play('misc/live_mode-feedback.mp3')
-        player.wait_for_playback()
 
 
 # required for live voice control
 def give_transcription_feedback():
     if len(config_manager.config['voice-feedback-transcription-capable-speeches']) == 0:
         return
+    global wroteTranscriptionSpeech
     if config_manager.config['voice-transcription-feedback-enabled']:
-        if internet:
+        if internet and not wroteTranscriptionSpeech:
             speech = speak(random.choice(config_manager.config['voice-feedback-transcription-capable-speeches']))
             if config_manager.config['voice-cache-enabled']:
                 speech.save('misc/transcription-feedback.mp3')
+                wroteTranscriptionSpeech = True
         elif os.path.exists('misc/transcription-feedback.mp3'):
             player.speed = config_manager.config['voice-feedback-speed']
+            player.af = f'rubberband=pitch-scale={config_manager.config["voice-pitch"]}'
             player.play('misc/transcription-feedback.mp3')
 
 
 # checks if network is reachable
 def check_network():
     try:
-        gTTS(text='knock knock', lang='en', slow=False).save('misc/network-test.mp3')
+        gTTS(text='checking network connectivity ...', lang='en', slow=False).save('misc/network-test.mp3')
         return True
     except gTTSError:
         return False
@@ -126,6 +145,7 @@ def greet():
             speech.save('misc/greeting.mp3')
     else:
         player.speed = config_manager.config['voice-feedback-speed']
+        player.af = f'rubberband=pitch-scale={config_manager.config["voice-pitch"]}'
         player.play('misc/greeting.mp3')
         player.wait_for_playback()
 
